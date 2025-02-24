29983ae2a157a67e841e7ee1998beff6b7117016
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-02-24 15:20:52 +0000

    arrumando mais coisinhas

--- Aula1-LAB1B/src/Main.java
+++ Aula1-LAB1B/src/Main.java
@@ -9,10 +9,10 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
     String texto = "";
     for (int i = 1; i <= 2; i++) {
-        System.out.print("\nInforme um texto: ");
+        System.out.print("\n\nPara sair do programa, digite /sair\n\nInforme um texto: ");
         texto = scanner.nextLine();
         if (texto.equals("/sair")) {
-            System.out.print("Você saiu do programa");
+            System.out.print("Você saiu do programa\n");
             break;
         } else {
             Palindromo teste = new Palindromo(texto);
